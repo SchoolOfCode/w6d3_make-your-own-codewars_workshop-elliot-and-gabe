8a4e5d472ace4bb210ca7bb893defb693ad98441
Elliott.Stevens <Elliott.Samuel.Stevens@gmail.com>
2021-12-22 19:00:52 +0000

    Mo' typos fixed in tests

--- main.test.js
+++ main.test.js
@@ -41,13 +41,13 @@ describe(`Cipher challenge`, () => {
     // Arrangement
     const cipher =
       "2ffdf11f22r5g32afqw__gww82_ife2!__5__s1_asome_gd2oo44w??d_nudfTmbtnd!er";
-    const scramledKey = [
+    const scrambledKey = [
       25, 13, 45, 40, 57, 64, 26, 69, 27, 11, 63, 49, 37, 56, 39, 50, 24, 70, 5,
       46, 6, 58, 59,
     ];
     const expected = "115382_is_a_good_number";
     // Act
-    const actual = decryptMessage(cipher, scramledKey);
+    const actual = decryptMessage(cipher, scrambledKey);
     // Assert
     expect(actual).toBe(expected);
   });
